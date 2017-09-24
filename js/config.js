38fbb07e8a23ace0f88eee87f94b6f8a83e8acb5
kim666166 <kim666166@gmail.com>
2017-09-24 13:20:58 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,127 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.08
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead
+(Put the files in the images folder)*/
+var l_serverImage = "logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "슈뢰딩거의 HL2RP 서버";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"aurora.webm"
+"bluefall.webm"
+"bluetilefloor.webm"
+"bluewhitecircles.webm"
+"bokehlens.webm"
+"cleanbokeh.webm"
+"coldnight.webm"
+"colorfulburst.webm"
+"dancingstars.webm"
+"dropsflares.webm"
+"focusorangebokeh.webm"
+"glimmering.webm"
+"glowingrings.webm"
+"greencircles.webm"
+"hues.webm"
+"inthedeep.webm"
+"modernflourish.webm"
+"orangeaura.webm"
+"warminside.webm"
+"yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format 
+(Put the files in the backgrounds/videos folder)*/
+var l_background = "glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them
+(Put the files in the backgrounds/images folder)*/
+var l_bgImages = [
+	"1.jpg",
+	"2.jpg",
+	"3.jpg",
+	"4.jpg",
+	"5.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much .ogg songs you want
+(Put the files in the songs folder)*/
+var l_musicPlaylist = [
+	{youtube: "iy61r3Qkm6o", name: "Half-Life 2 - Triage at Dawn |Remix| Path of Borealis"},
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+	"당신은 여기서 생각보다 강하지 않습니다.",
+	"잘못을 했다면 이미 늦었습니다.",
+	"복종하십시오.",
+	"굴복해야만 살아갈 수 있습니다.",
+	"어둠 속엔 빛이 항상 있습니다.",
+	"당신을 믿지 마세요.",
+
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
